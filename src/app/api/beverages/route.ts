0ef1f54142016ba1f506b11cbe03b2d5fe5926d7
Jaro <jaroslavfilo95@gmail.com>
2026-06-13 18:08:39 +0200

    initial setup

---
+++ src/app/api/beverages/route.ts
@@ -0,0 +1,12 @@
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+
+export async function GET(): Promise<Response> {
+  const { env } = getCloudflareContext();
+
+  const { results } = await env.jaroslavfilo_db
+    .prepare("SELECT * FROM Customers WHERE CompanyName = ?")
+    .bind("Bs Beverages")
+    .all();
+
+  return Response.json(results);
+}
